2f9e6fb9aeec4f1ed3f517d561712d1f120acdd9
Biplab-Roy <47073892+Biplab-Roy@users.noreply.github.com>
2022-07-08 18:29:59 +0530

    Initial Commit

---
+++ precache-manifest.1b4b76651a4b3b293d3171cdda1ab6aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "30cdd5f9fc69c5b771b1",
+    "url": "/Portfolio/static/css/main.a5178cf4.chunk.css"
+  },
+  {
+    "revision": "30cdd5f9fc69c5b771b1",
+    "url": "/Portfolio/static/js/main.30cdd5f9.chunk.js"
+  },
+  {
+    "revision": "595d6648e0f00f961483",
+    "url": "/Portfolio/static/js/1.595d6648.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "096b9ea9f589c991257ad37535285d69",
+    "url": "/Portfolio/static/media/CV.096b9ea9.pdf"
+  },
+  {
+    "revision": "f6923584732bd3eab3218d266aca6a07",
+    "url": "/Portfolio/static/media/me.f6923584.png"
+  },
+  {
+    "revision": "5ce945d982c4ff4dd4fb62584f3c12f9",
+    "url": "/Portfolio/static/media/image.5ce945d9.jpg"
+  },
+  {
+    "revision": "c83226359440766ae0641fbc164cdd54",
+    "url": "/Portfolio/static/media/sortingVisualization.c8322635.jpeg"
+  },
+  {
+    "revision": "fcd21ff7450ecf5b39b958a9c4bd6a52",
+    "url": "/Portfolio/static/media/spamDetectionModel.fcd21ff7.jpeg"
+  },
+  {
+    "revision": "c258a4163cd0d6bced8bba02c914bc72",
+    "url": "/Portfolio/static/media/handwrittenDigitClassifier.c258a416.jpeg"
+  },
+  {
+    "revision": "5bf1b0048103cdc1550febb5392c27e1",
+    "url": "/Portfolio/static/media/durgaDarshan.5bf1b004.jpg"
+  },
+  {
+    "revision": "7420cf3dcb30461909a59a14096d7f08",
+    "url": "/Portfolio/index.html"
+  }
+];
